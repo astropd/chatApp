637608dba5e65e095f6cba6e8abeec02aeb50216
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 01:11:31 +0900

    change app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
 
 
